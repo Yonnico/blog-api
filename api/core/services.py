5d92474cde59c902125482f87b67ae588ce53ec8
YoungMorzh <pancake.00@mail.ru>
2022-08-04 22:34:06 +0300

    change core/services.py

--- api/core/services.py
+++ api/core/services.py
@@ -2,11 +2,15 @@ from flask import request, abort
 
 def validate_len(val):
     if len(val) == 0:
-        abort(404)
+        return abort(404)
     return
 
 
 def validate_request(val):
     if val not in request.json:
-        abort(400)
+        return abort(400)
     return
+
+
+def validate_for_str(val):
+    return isinstance(val, str) and len(val)
